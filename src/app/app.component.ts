f5c500eeef9716f19ac89f60853f3bb0c698ea85
unknown <lakshman.avula@outlook.com>
2018-08-29 18:36:44 +0530

    added new pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wcc';
+  up_icon; down_icon; isSelected; feedback; message;successMessage;
+  ws_name = "Workshop on CSS and JS";
+  constructor(){
+    this.initializeValues()
+  }
+  happy(event){
+    if(!this.isSelected){
+       this.up_icon="fa fa-smile-o";
+       this.isSelected = true;
+       this.feedback = "1";
+    }
+  }
+  notHappy(event){
+    if(!this.isSelected){
+       this.down_icon="fal fa fa-frown-o";
+       this.isSelected = true;
+       this.feedback = "0";
+    }
+  }
+  onSubmit(){
+    
+    this.successMessage = "Thanks for your valuable Feedback";
+    this.initializeValues()
+    setTimeout(()=>{
+      this.successMessage=""
+    },2000)
+
+
+  }
+  getStatusValue(){
+    return this.feedback == "" ? true : false;
+  }
+  initializeValues(){
+    this.isSelected = false;
+    this.feedback = "";
+    this.message = "";
+    this.up_icon="fa fa-thumbs-o-up";
+    this.down_icon="fa fa-thumbs-o-down";
+   
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbPaginationModule, NgbAlertModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
